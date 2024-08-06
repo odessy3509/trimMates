dfd7c6c277be250b99502a674f317ef7de7b7eac
odessy <137520021+odessy3509@users.noreply.github.com>
2024-08-06 18:57:58 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,36 +1,30 @@
-// Get the modal
-var modalContainer = document.getElementById("modalContainer");
+// JavaScript for handling the modal
+document.addEventListener('DOMContentLoaded', function() {
+    var modal = document.getElementById("bookingModal");
+    var btn = document.getElementById("bookNowBtn");
+    var span = document.getElementsByClassName("close")[0];
+    var serviceSelect = document.getElementById("service");
+    var customServiceInput = document.getElementById("customService");
 
-// Load the modal HTML content
-fetch('modal.html')
-    .then(response => response.text())
-    .then(data => {
-        modalContainer.innerHTML = data;
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
 
-        // Get the modal
-        var modal = document.getElementById("myModal");
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
 
-        // Get the button that opens the modal
-        var btn = document.getElementById("openModal");
-
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks the button, open the modal
-        btn.onclick = function() {
-            modal.style.display = "block";
-        }
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
+    window.onclick = function(event) {
+        if (event.target == modal) {
             modal.style.display = "none";
         }
+    }
 
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
+    serviceSelect.addEventListener('change', function() {
+        if (this.value === "custom") {
+            customServiceInput.style.display = "block";
+        } else {
+            customServiceInput.style.display = "none";
         }
-    })
-    .catch(error => console.error('Error loading the modal:', error));
+    });
+});

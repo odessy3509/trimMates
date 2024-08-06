be6fb02bf93c3d432ebe465ccdec25e2bfc7de45
odessy <137520021+odessy3509@users.noreply.github.com>
2024-08-05 21:41:56 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -23,15 +23,4 @@ document.addEventListener('DOMContentLoaded', () => {
         modal.style.display = 'none';
         document.getElementById('bookingForm').reset();
     });
-
-    // Add smooth scrolling to the footer when clicking the 'Contact' button
-document.querySelector('a[href="#footer"]').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent default anchor behavior
-
-    // Get the footer element
-    const footer = document.getElementById('footer');
-    
-    // Scroll to the footer with smooth behavior
-    footer.scrollIntoView({ behavior: 'smooth' });
-});
 });
